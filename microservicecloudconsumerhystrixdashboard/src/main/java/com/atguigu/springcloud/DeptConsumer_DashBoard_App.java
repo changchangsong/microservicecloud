bb4b76b49baeca0c631e1273cbe6294ac4acffa0
songchang <1334452063@qq.com>
2020-04-20 20:12:38 +0800

    代码

---
+++ microservicecloudconsumerhystrixdashboard/src/main/java/com/atguigu/springcloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by songchang on 2020/4/19.
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DeptConsumer_DashBoard_App
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DeptConsumer_DashBoard_App.class,args);
+    }
+}
+
+
